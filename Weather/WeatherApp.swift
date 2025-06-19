cc6bcd0661da2a2ba768d8c8dcfdb7e00a647877
Shishmakova Daria <dary_shishka@mail.ru>
2025-06-19 17:02:03 +0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Дарья Шишмакова on 19.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
