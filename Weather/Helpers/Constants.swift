35f39dbb91603fc80287ac2ddfbd42813a4b5c5d
Shishmakova Daria <dary_shishka@mail.ru>
2025-06-20 08:33:46 +0300

    changed settings

--- Weather/Helpers/Constants.swift
+++ Weather/Helpers/Constants.swift
@@ -10,7 +10,7 @@ import Foundation
 enum Constants {
     
     // TODO: вставьте сюда ваш API ключ
-    static let apiKey = "59caef7dd0d84cad986122644251906"
+    static let apiKey = "YOUR_API_KEY"
     
     static let defaultCity = "saint-petersburg"
 }
